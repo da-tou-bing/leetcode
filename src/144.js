95cb24bb387b507c00b8d74ea76a866eca5d7cac
da-tou-bing <ideago63@qq.com>
2020-08-25 18:19:24 +0800

    144提交

---
+++ src/144.js
@@ -0,0 +1,60 @@
+/**
+ * 给定一个二叉树，返回它的 前序 遍历。
+
+ 示例:
+
+输入: [1,null,2,3]  
+   1
+    \
+     2
+    /
+   3 
+
+输出: [1,2,3]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+var preorderTraversal = function (root) {
+  const res = [];
+  traversal = (root) => {
+    root && res.push(root.val);
+    root && root.left && traversal(root.left);
+    root && root.right && traversal(root.right);
+  }
+  traversal(root);
+  console.log(`res`,res);
+  return res;
+};
+
+const root = {
+  val: 1,
+  left: null,
+  right: {
+    val: 2,
+    left: {
+      val: 3,
+      left: null,
+      right: null
+    },
+    right: null
+  }
+};
+
+preorderTraversal(root);
